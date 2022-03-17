3b249da015966eabee1c19f41cf43f4e0c8a5012
Daniela Monteiro <danimdf360@gmail.com>
2022-03-17 20:03:00 -0300

    refactor: Resolve exercism 02!

--- Desafio-GFT/src/exercicio02/Main.java
+++ Desafio-GFT/src/exercicio02/Main.java
@@ -2,11 +2,28 @@ package exercicio02;
 import java.util.Scanner;
 
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
+        String dig = Integer.toString(num);
+        char numArr[] = dig.toCharArray();
+        int numOfArr = numArr.length;
+        int count_impar = 0;
 
-        System.out.println("São " + num + " numeros impares");
+        if (numOfArr <= 2) {
+            System.out.println("Número inválido! Por favor digite um número com 3 ou 4 digitos!!");
+            return;
+        } else if (num <= 350 || num >= 8750) {
+            System.out.println("Número Inválido!");
+            return;
+        }
+        for (int i = 0; i < numOfArr; i++) {
+
+            if (numArr[i] % 2 == 1) {
+                count_impar++;
+            }
+        }
+        System.out.println("São " + count_impar + " numeros impares");
     }
 }
